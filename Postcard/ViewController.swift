6a47cab33f3d5df8868c156c7405ae853176ed3a
Mike Craig <mikecraig@earthlink.net>
2015-09-04 20:19:47 -0500

    Added Name Label & actions

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var sendMessageButton: UIButton!
@@ -26,12 +27,21 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMessageButtonPressed(sender: AnyObject) {
+        // Handle when the Send Message button is pushed.
+        
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         
         sendMessageButton.setTitle("Mail Sent", forState: UIControlState.Normal)
         
